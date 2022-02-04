963b51620a5d726d233f02f7975f4749e93e8449
Anna Kummer <annamkummer@gmail.com>
2022-02-03 18:06:19 -0700

    Add nav bar with links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './scss/App.scss';
+import Nav from './views/Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          KVIZO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
   );
 }
---
+++ src/views/Nav.js
@@ -0,0 +1,16 @@
+import '../scss/Nav.scss';
+import { NavLink } from 'react-router-dom'
+
+function Nav() {
+  return (
+    <div className="nav">
+      <h1>Kvizo</h1>
+      <NavLink to="/" className="home">Home</NavLink>
+      <NavLink to="/create" className="create">Create Quiz</NavLink>
+      <NavLink to="/quizzes" className="quizzes">Quizzes</NavLink>
+      <NavLink to="/about" className="about">About us</NavLink>
+    </div>
+  );
+}
+
+export default Nav;
