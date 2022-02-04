645de069f987108b5cf2407000a8a00aa19646ca
Anna Kummer <annamkummer@gmail.com>
2022-02-04 14:31:15 -0700

    Update home link class name

--- src/views/Nav.js
+++ src/views/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
   return (
     <div className="nav">
       <h1>Kvizo</h1>
-      <NavLink to="/" className="home">Home</NavLink>
+      <NavLink to="/" className="home-link">Home</NavLink>
       <NavLink to="/create" className="create">Create Quiz</NavLink>
       <NavLink to="/quizzes" className="quizzes">Quizzes</NavLink>
       <NavLink to="/about" className="about">About us</NavLink>
