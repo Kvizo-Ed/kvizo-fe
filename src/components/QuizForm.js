3bf9ea1afa62a6e5919e9ed174b770c68c5a2773
Anna Kummer <annamkummer@gmail.com>
2022-02-06 11:45:28 -0700

    Add info icon and random answer message

--- src/components/QuizForm.js
+++ src/components/QuizForm.js
@@ -1,5 +1,10 @@
 import '../scss/QuizForm.scss';
 import { useState } from 'react'
+import { FaInfoCircle } from 'react-icons/fa'
+import { RiCloseCircleFill } from 'react-icons/ri'
+import Modal from 'react-modal'
+
+Modal.setAppElement(document.getElementById('root'))
 
 function QuizForm({ content, setContent }) {
 
@@ -40,12 +45,29 @@ function QuizForm({ content, setContent }) {
         })
     }
 
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    const toggleInfoModal = (e) => {
+        e.preventDefault()
+        setModalIsOpen(!modalIsOpen)
+    }
+
     return (
         <form className="create-quiz">
             <div className="quiz-form-question">
                 <h1 className="quiz-form-question-number">{content.questions.length + 1}.</h1>
                 <label className="quiz-form-question-label">Question</label>
                 <textarea className="quiz-form-question-input" type="text" name="questionText" value={question.questionText} onChange={(e) => handleChange(e)} />
+            </div>
+
+            <div className="info-btn-container">
+                <button onClick={(e) => toggleInfoModal(e)} className="info-btn"><FaInfoCircle /></button>
+                <Modal isOpen={modalIsOpen} className="info-modal">
+                    <div className="info-modal-content">
+                        <button onClick={(e) => toggleInfoModal(e)} className="close-modal"><RiCloseCircleFill /></button>
+                        <h3>Multiple choice answer options will be randomized</h3>
+                    </div>
+                </Modal>
             </div>
 
             <div className="quiz-form-answer-row">
@@ -59,7 +81,7 @@ function QuizForm({ content, setContent }) {
                     <input className="quiz-form-answer-input" type="text" name="possibleAnswerB" value={question.possibleAnswerB} onChange={(e) => handleChange(e)} />
                 </div>
             </div>
-            
+
             <div className="quiz-form-answer-row">
                 <div className="form-answer">    
                     <label className="quiz-form-answer-label incorrect-label">Incorrect Answer 2</label>
