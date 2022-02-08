7c1bbf90fe7f282517b53c3f765d741a8e386540
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-02-08 09:45:16 -0700

    Add routes to index

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './scss/App.scss';
 import Home from './views/Home';
 import Nav from './views/Nav'
-import { Routes, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import CreateMultiForm from './views/CreateMultiForm'
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/create/*" element={ <CreateMultiForm />} />
-      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <App />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
