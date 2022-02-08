b030fc8df80d77d6b131187c4cf2d063fa90ab18
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-02-08 09:55:36 -0700

    Move routes backl

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './scss/App.scss';
 import Home from './views/Home';
 // import Nav from './views/Nav'
-import { Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import CreateMultiForm from './views/CreateMultiForm'
 
 function App() {
   return (
     <div className="App">
       {/* <Nav /> */}
+      <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/create/*" element={ <CreateMultiForm />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <App />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
