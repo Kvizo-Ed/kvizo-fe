e4aa4c7a3fd6c40ca722aed614339ad6da029ab3
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-02-08 09:52:55 -0700

    remove nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './scss/App.scss';
 import Home from './views/Home';
-import Nav from './views/Nav'
+// import Nav from './views/Nav'
 import { Route } from 'react-router-dom'
 import CreateMultiForm from './views/CreateMultiForm'
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      {/* <Nav /> */}
         <Route path="/" element={ <Home /> } />
         <Route path="/create/*" element={ <CreateMultiForm />} />
     </div>
