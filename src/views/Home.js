ff8539aa937937de0e59b5067da16c5363ce3cdd
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-02-09 16:42:44 -0700

    Add sass vars

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,14 +2,13 @@ import '../scss/Home.scss';
 import multipleChoice from '../assets/multiple-choice.png';
 import bingo from '../assets/bingo.png';
 import { Link } from 'react-router-dom'
-// import PropTypes from 'prop-types';
 
 const Home = () => {
 
 	return (
 		<section className='home'>
 			<div className='home-greeting-container'>
-				<p className='home-greeting'>The worlds most advanced quiz creator. Utilizing nano technology we are able to inject the knowlege directly into your students brain.</p>
+				<p className='home-greeting'>PlaceHolder - The worlds most advanced quiz creator. Utilizing nano technology we are able to inject the knowlege directly into your students brain.</p>
 			</div>
 			<div className='build-quiz-container'>
 				<label className='button-title' >Build a <br/>quiz?</label>
@@ -23,10 +22,3 @@ const Home = () => {
 }
 
 export default Home;
-
-// Home.propTypes = {
-//   data: PropTypes.object,
-//   handleSubClick: PropTypes.func,
-//   handleFavClick: PropTypes.func,
-//   subscriptions: PropTypes.object
-// };
