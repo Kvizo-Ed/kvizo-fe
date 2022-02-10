eada466baaa3229ac410222cebbee06802eb7ed3
Anna Kummer <annamkummer@gmail.com>
2022-02-09 20:14:44 -0700

    Create Bingo form files

---
+++ src/components/BingoForm.js
@@ -0,0 +1,12 @@
+import '../scss/BingoForm.scss';
+
+function BingoForm() {
+
+    return (
+        <form className="create-bingo">
+            <h1>Bingo Form</h1>
+        </form>
+    );
+}
+
+export default BingoForm;
