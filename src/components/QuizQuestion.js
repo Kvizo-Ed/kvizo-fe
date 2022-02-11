09b20c1cfbee4d0d8dfb60caaf2aacbe24b6e580
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-02-11 14:21:06 -0700

    Remove comments

--- src/components/QuizQuestion.js
+++ src/components/QuizQuestion.js
@@ -5,14 +5,10 @@ import { Link } from 'react-router-dom'
 
 function QuizQuestion({ quiz }) {
 
-	let { id } = useParams();
-
+	let { id } = useParams()
 	let parsedId = parseInt(id)
-
 	let navigate = useNavigate()
-
 	let quizQuestion = quiz.attributes.questions[parsedId - 1]
-	console.log(quiz.attributes.questions.length)
 
 	let answers = quizQuestion.possibleAnswers.map((element, index) => {
 		return (
@@ -25,13 +21,13 @@ function QuizQuestion({ quiz }) {
 
 	return (
 		<section className="quiz-question-container">
-				<button disabled={parsedId - 1 == 0} onClick={() => {navigate(`/quiz/${quiz.id}/question/${parsedId - 1}`)}}>Prev</button>
+			<button disabled={parsedId - 1 == 0} onClick={() => {navigate(`/quiz/${quiz.id}/question/${parsedId - 1}`)}}>Prev</button>
 			<div className='quiz-question' key={id}>
 				<div className='quiz-question-description'>
 					<p>{quizQuestion.questionText}</p>
 				</div>
 				<div className='answers-container'>
-						{answers}
+					{answers}
 				</div>
 			</div>
 			<button disabled={parsedId + 1 > quiz.attributes.questions.length} onClick={() => {navigate(`/quiz/${quiz.id}/question/${parsedId + 1}`)}}>Next</button>
