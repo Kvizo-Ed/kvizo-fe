bed25929a76bb215dca6da7e66783cf067dbe341
Anna Kummer <annamkummer@gmail.com>
2022-02-13 13:26:24 -0700

    Install actioncable provider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ActionCableProvider url={API_WS_ROOT}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </ActionCableProvider>,
   document.getElementById('root')
 );
 
