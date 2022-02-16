3bfd2298df8ad575036239afc338acd692d60270
Anna Kummer <annamkummer@gmail.com>
2022-02-16 12:34:22 -0700

    Add tests for home view

---
+++ cypress/integration/home_spec.js
@@ -0,0 +1,26 @@
+
+
+describe('Home View', () => {
+
+    beforeEach(() => {
+      cy.visit('localhost:3000')
+    })
+  
+    it('As a user, when I visit the home page, I should see a navigation bar with links to Home, Create, Quizzes, and About pages.', () => {
+      cy.get('.navbar').contains('Home')
+      cy.get('.navbar').contains('Create')
+      cy.get('.navbar').contains('Quizzes')
+      cy.get('.navbar').contains('About')
+    })
+  
+    it('As a user, when I visit the home page, I should see a Build a quiz? section and be able to click the Multiple Choice icon.', () => {
+      cy.get('.button-title').contains('Build a quiz?')
+    })
+  
+    it('As a user, if I visit a url that does not exist, I should see a page not found message.', () => {
+      cy.visit('localhost:3000/cheese')
+      cy.get('.error-view').contains("Hmm")
+    })
+
+  })
+  
