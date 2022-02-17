d4b47f631e2fcfbe1095a9ee3b700546001636fb
Anna Kummer <annamkummer@gmail.com>
2022-02-16 17:42:22 -0700

    Add to Create test beforeEaches

--- cypress/integration/create_spec.js
+++ cypress/integration/create_spec.js
@@ -1,4 +1,4 @@
-describe('Create View', () => {
+describe('Create View: New Quiz', () => {
 
     beforeEach(() => {
       cy.visit('localhost:3000/create')
@@ -31,21 +31,6 @@ describe('Create View', () => {
         cy.get('.create').click()
     })
   
-    it('As a user, when I click the Create Quiz! button, I should see the title I just input and a form to input a question, correct answer, and three incorrect answers.', () => {
-        cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
-        cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
-        cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
-        cy.get('.quiz-form-header-input[name="grade"]').select(8)
-        cy.get('.create').click()
-
-        cy.get('.quiz-form-info-title').contains("Cypress Quiz Title")
-        cy.get('form')
-        cy.get('.quiz-form-answer-label').contains('label', "Correct Answer")
-        cy.get('.quiz-form-answer-label').contains('label', "Incorrect Answer 1")
-        cy.get('.quiz-form-answer-label').contains('label', "Incorrect Answer 2")
-        cy.get('.quiz-form-answer-label').contains('label', "Incorrect Answer 3")
-    })
-  
     it('As a user, when I click the Create Quiz! button, if the quiz does not save, I should see an error message.', () => {
         cy.intercept('POST', "https://kvizo-be.herokuapp.com//api/v1/quizzes", {
             statusCode: 404,
@@ -60,57 +45,56 @@ describe('Create View', () => {
         cy.get('.error-view').contains("Oops!")
     })
     
-    it('As a user, when I click the Create Quiz! button, if I click the Save Quiz button whithout completing all fields, I should see an error message and the missing fields should be highlighted.', () => {
+})
+
+describe('Create View: Add questions', () => {
+
+    beforeEach(() => {
+        cy.visit('localhost:3000/create')
         cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
         cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
         cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
         cy.get('.quiz-form-header-input[name="grade"]').select(8)
         cy.get('.create').click()
 
+        cy.get('.quiz-form-question-input').type("Cypress Question 1")
+        cy.get('.quiz-form-answer-input[name="correctAnswer"]').type("Cypress Correct Answer")
+        cy.get('.quiz-form-answer-input[name="possibleAnswerB"]').type("Cypress Incorrect Answer 1")
+        cy.get('.quiz-form-answer-input[name="possibleAnswerC"]').type("Cypress Incorrect Answer 2")
+        cy.get('.quiz-form-answer-input[name="possibleAnswerD"]').type("Cypress Incorrect Answer 3")
+      })
+
+      it('As a user, when I click the Create Quiz! button, I should see the title I just input and a form to input a question, correct answer, and three incorrect answers.', () => {
+        cy.get('.quiz-form-info-title').contains("Cypress Quiz Title")
+        cy.get('form')
+        cy.get('.quiz-form-answer-label').contains('label', "Correct Answer")
+        cy.get('.quiz-form-answer-label').contains('label', "Incorrect Answer 1")
+        cy.get('.quiz-form-answer-label').contains('label', "Incorrect Answer 2")
+        cy.get('.quiz-form-answer-label').contains('label', "Incorrect Answer 3")
+    })
+
+      it('As a user, when I click the Create Quiz! button, if I click the Save Quiz button whithout completing all fields, I should see an error message and the missing fields should be highlighted.', () => {
+        cy.get('.quiz-form-answer-input[name="possibleAnswerD"]').clear()
         cy.get('.save-question').click()
         cy.get('.show').contains("Please complete all fields")
         cy.get('.show-0')
     })
         
     it('As a user, when I click the Create Quiz! button, I should see an info icon that opens a modal explaining that responses are randomized.', () => {
-        cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
-        cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
-        cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
-        cy.get('.quiz-form-header-input[name="grade"]').select(8)
-        cy.get('.create').click()
-
         cy.get('.info-btn').click()
         cy.get('h3').contains("Multiple choice answer options will be randomized")
         
     })
         
     it('As a user, I should be able to close the modal by clicking the X.', () => {
-        cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
-        cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
-        cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
-        cy.get('.quiz-form-header-input[name="grade"]').select(8)
-        cy.get('.create').click()
-
         cy.get('.info-btn').click()
         cy.get('.close-modal').click()
         cy.get('.quiz-form-title-input')
     })
             
     it('As a user, when I click the Create Quiz! button, input all fields, and click Save Question, I should see all fields reset and the question number advance.', () => {
-        cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
-        cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
-        cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
-        cy.get('.quiz-form-header-input[name="grade"]').select(8)
-        cy.get('.create').click()
-
         cy.get('.quiz-form-question-number').contains('1')
-        
-        cy.get('.quiz-form-question-input').type("Cypress Question 1")
-        cy.get('.quiz-form-answer-input[name="correctAnswer"]').type("Cypress Correct Answer")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerB"]').type("Cypress Incorrect Answer 1")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerC"]').type("Cypress Incorrect Answer 2")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerD"]').type("Cypress Incorrect Answer 3")
-        
+
         cy.get('.quiz-form-answer-input[name="correctAnswer"]').should('have.value', "Cypress Correct Answer")
         cy.get('.quiz-form-answer-input[name="possibleAnswerB"]').should('have.value', "Cypress Incorrect Answer 1")
         cy.get('.quiz-form-answer-input[name="possibleAnswerC"]').should('have.value', "Cypress Incorrect Answer 2")
@@ -125,18 +109,6 @@ describe('Create View', () => {
     })
                 
     it('As a user, when I click the Create Quiz! button, I should be able to click the Preview button and see the quesiton I input and answers in randomized order.', () => {
-        cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
-        cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
-        cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
-        cy.get('.quiz-form-header-input[name="grade"]').select(8)
-        cy.get('.create').click()
-
-        cy.get('.quiz-form-question-input').type("Cypress Question 1")
-        cy.get('.quiz-form-answer-input[name="correctAnswer"]').type("Cypress Correct Answer")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerB"]').type("Cypress Incorrect Answer 1")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerC"]').type("Cypress Incorrect Answer 2")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerD"]').type("Cypress Incorrect Answer 3")
-        
         cy.get('.preview').click()
 
         cy.get('.quiz-question-description').contains("Cypress Question 1")
@@ -147,24 +119,31 @@ describe('Create View', () => {
     })
                 
     it('As a user, I should be able to close the preview by clicking the X.', () => {
-        cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
-        cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
-        cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
-        cy.get('.quiz-form-header-input[name="grade"]').select(8)
-        cy.get('.create').click()
-
-        cy.get('.quiz-form-question-input').type("Cypress Question 1")
-        cy.get('.quiz-form-answer-input[name="correctAnswer"]').type("Cypress Correct Answer")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerB"]').type("Cypress Incorrect Answer 1")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerC"]').type("Cypress Incorrect Answer 2")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerD"]').type("Cypress Incorrect Answer 3")
-        
         cy.get('.preview').click()
         cy.get('.close-btn').click()
         cy.get('.quiz-form-question-input')       
     })
-                
+  
+    it('As a user, when I click the Create Quiz! button, input all fields, and click Save Question, I should see a Finish Creating Quiz button appear.', () => {
+        cy.get('.save-question').click()
+
+        cy.get('.btn').contains("Finish Creating Quiz")
+    })
+  
+    it('As a user, when i click the Finish Creating Quiz button, I should be taken to the quiz view for that new quiz.', () => {
+        cy.get('.save-question').click()
+
+        cy.get('.btn').contains("Finish Creating Quiz").click()
+        cy.url().should('include', 'quiz')
+        cy.get('.take-quiz').contains("Take Quiz")
+    })
+
+})
+
+describe('Create View: Question Error', () => {
+                    
     it('As a user, when I click the Create Quiz! button, input all fields, and click Save Question, if the question does not save, I should see an error message and the question number should not advance.', () => {
+        cy.visit('localhost:3000/create')
         cy.intercept('PATCH', "", {
             statusCode: 404,
             ok: false
@@ -185,43 +164,6 @@ describe('Create View', () => {
 
         cy.get('.error-view').contains("Oops!")
     })
-  
-    it('As a user, when I click the Create Quiz! button, input all fields, and click Save Question, I should see a Finish Creating Quiz button appear.', () => {
-        cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
-        cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
-        cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
-        cy.get('.quiz-form-header-input[name="grade"]').select(8)
-        cy.get('.create').click()
 
-        cy.get('.quiz-form-question-input').type("Cypress Question 1")
-        cy.get('.quiz-form-answer-input[name="correctAnswer"]').type("Cypress Correct Answer")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerB"]').type("Cypress Incorrect Answer 1")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerC"]').type("Cypress Incorrect Answer 2")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerD"]').type("Cypress Incorrect Answer 3")
-        cy.get('.save-question').click()
-
-        cy.get('.btn').contains("Finish Creating Quiz")
-    })
-  
-    it('As a user, when i click the Finish Creating Quiz button, I should be taken to the quiz view for that new quiz.', () => {
-        cy.get('.quiz-form-title-input').type("Cypress Quiz Title")
-        cy.get('.quiz-form-header-input[name="subject"]').type("Cypress Quiz Subject")
-        cy.get('.quiz-form-header-input[name="topic"]').type("Cypress Quiz Topic")
-        cy.get('.quiz-form-header-input[name="grade"]').select(8)
-        cy.get('.create').click()
-
-        cy.get('.quiz-form-question-input').type("Cypress Question 1")
-        cy.get('.quiz-form-answer-input[name="correctAnswer"]').type("Cypress Correct Answer")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerB"]').type("Cypress Incorrect Answer 1")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerC"]').type("Cypress Incorrect Answer 2")
-        cy.get('.quiz-form-answer-input[name="possibleAnswerD"]').type("Cypress Incorrect Answer 3")
-        cy.get('.save-question').click()
-
-        cy.get('.btn').contains("Finish Creating Quiz").click()
-        cy.url().should('include', 'quiz')
-        cy.get('.take-quiz').contains("Take Quiz")
-
-    })
-
-  })
+})
   
