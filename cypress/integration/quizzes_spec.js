b565d0d765a3943a358c482752d92bc2be047c03
Anna Kummer <annamkummer@gmail.com>
2022-02-17 08:08:46 -0700

    Begin Quizzes tests

---
+++ cypress/integration/quizzes_spec.js
@@ -0,0 +1,24 @@
+describe('Quizzes View', () => {
+
+    beforeEach(() => {
+      cy.visit('localhost:3000')
+    })
+  
+    it('As a user, when I visit the quizzes page, if the quizzes are not able to load, I should see an error message.', () => {
+
+    })
+  
+    it('As a user, when I visit the quizzes page, I should see a Loading message until the quizzes appear.', () => {
+
+    })
+  
+    it('As a user, when I visit the quizzes page, after the quizzes load, I should see the title, subject, topic, and grade level for each quiz.', () => {
+
+    })
+  
+    it('As a user, when I visit the quizzes page, I should be able to click a quiz and be taken to that quiz page .', () => {
+
+    })
+
+  })
+  
